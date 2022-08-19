acf2ce144374c1441d01e3175012fb1d5e291995
Rahul Gupta <thesoulmate3@gmail.com>
2022-08-19 23:08:40 +0530

    Refactor previous changes

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -3,39 +3,38 @@ module Api
     module Users
       # Manages {User} information related to {devise-api}
       class RegistrationsController < ApplicationController
-
         # Creates new User
         def create
           user = User.new(user_params)
           if user.save
-            render json: { 
+            render json: {
               status: 201,
               user: user,
               message: 'User registered successfully'
             }
           else
             render status: 400,
-              json: { 
-                message: user.errors.full_messages 
-              }
+                   json: {
+                     message: user.errors.full_messages
+                   }
           end
         end
-        
-        # Verifies for the user 
+
+        # Verifies for the user
         def verify
           user = User.find_by_email user_params[:email]
           if user&.valid_password?(user_params[:password])
             render status: 200,
-              json: { 
-                message: 'True, user was found',
-                user: user
-              }
+                   json: {
+                     message: 'True, user was found',
+                     user: user
+                   }
           else
             render status: 400,
-              json: { 
-                status: 404,
-                message: 'False, something went wrong or user not found',
-              }
+                   json: {
+                     status: 404,
+                     message: 'False, something went wrong or user not found'
+                   }
           end
         end
 
